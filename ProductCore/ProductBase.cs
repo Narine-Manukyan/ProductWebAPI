a41d52176ce56821b18a489c2f4666ebb4ba4d03
Narine Manukyan <narine.manukyan@praemium.com>
2021-08-25 21:04:42 +0400

    initial changes

--- ProductCore/ProductBase.cs
+++ ProductCore/ProductBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ProductCore
@@ -14,5 +15,7 @@ namespace ProductCore
         public string Name { get; set; }
         public double Price { get; set; }
         public bool Available { get; set; }
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
     }
 }
---
+++ ProductData/Migrations/20210825141620_RowVersion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductData.Migrations
+{
+    public partial class RowVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Products",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Products");
+        }
+    }
+}
+
+
